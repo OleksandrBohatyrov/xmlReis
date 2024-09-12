a7504a21ce3818118716fc5ca8223eaca5387027
OleksandrBohatyrov <120181397+OleksandrBohatyrov@users.noreply.github.com>
2024-09-12 14:34:22 +0300

    faili Reis.sql loomine

---
+++ Reis.sql
@@ -0,0 +1,89 @@
+create table Reis (
+	ID INT,
+	Lennujaam VARCHAR(50),
+	Transport VARCHAR(9),
+	Kuupaev DATE,
+	Hind INT,
+	Algus_Koht VARCHAR(50),
+	Lõpp_Koht VARCHAR(50),
+	Firma VARCHAR(50),
+	FirmaRating INT,
+	Ekskursioonid INT,
+	Majutus INT
+);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (1, 'Sibi Airport', 'Alaska', '2024-05-23 12:51:50', 128, 'Itzer', 'Wulijie', 'Chatterbridge', 4, 803, 859);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (2, 'Catania-Fontanarossa Airport', 'Spirit', '2024-04-24 22:15:15', 1907, 'Rizal', 'Dongyang', 'Zava', 5, 389, 3216);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (3, 'Adirondack Regional Airport', 'JetBlue', '2024-08-29 16:46:13', 406, 'Kudang', 'Tuupovaara', 'Eimbee', 5, 328, 2875);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (4, 'Makoua Airport', 'Spirit', '2024-03-01 11:03:38', 526, 'Ban Phai', 'Al Jumaylīyah', 'Wordtune', 5, 895, 2853);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (5, 'Medina Airport', 'Southwest', '2024-07-10 10:56:56', 939, 'Sison', 'Mozi', 'Tazz', 5, 739, 1543);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (6, 'Prince Abdulmohsin Bin Abdulaziz Airport', 'United', '2024-09-09 10:31:11', 309, 'Hallsberg', 'Kujang', 'Dynava', 1, 353, 1243);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (7, 'Singleton Airport', 'United', '2024-07-23 18:52:46', 350, 'Yiyang', 'Palkovice', 'Flipopia', 4, 883, 1135);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (8, 'Marseille Provence Airport', 'American', '2024-07-11 17:36:01', 1861, 'Muyudian', 'Xiatang', 'Layo', 2, 384, 4168);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (9, 'Gusap Airport', 'Southwest', '2024-06-18 09:50:32', 1900, 'Dongqinggou', 'Banjar Buahan', 'Jaxbean', 4, 219, 2074);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (10, 'Linköping City Airport', 'American', '2024-08-04 06:23:54', 841, 'Ta’ Xbiex', 'Ouaoula', 'Gigabox', 1, 277, 3820);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (11, 'Tribhuvan International Airport', 'United', '2024-08-01 22:15:28', 1323, 'Vila Franca de Xira', 'Drenova', 'Twitterbeat', 1, 321, 2040);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (12, 'Samjiyŏn Airport', 'Southwest', '2024-07-11 14:39:52', 872, 'Jutiapa', 'Aioi', 'Gigabox', 3, 797, 1791);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (13, 'Hatzfeldhaven Airport', 'United', '2023-11-22 16:41:00', 1593, 'San Nicolas', 'Adrasmon', 'Browsezoom', 1, 617, 4317);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (14, 'Sultan Aji Muhamad Sulaiman Airport', 'Alaska', '2024-04-25 01:40:15', 1611, 'Skien', 'Suvorovo', 'Jaxworks', 2, 404, 1455);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (15, 'Kebar Airport', 'United', '2024-06-24 06:21:43', 341, 'Hekou', 'Bogale', 'Dabshots', 2, 172, 4435);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (16, 'Auxerre-Branches Airport', 'Spirit', '2023-10-13 05:35:39', 873, 'Przybyszówka', 'Banjar Bias', 'Wordify', 3, 355, 2757);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (17, 'Banja Luka International Airport', 'Spirit', '2023-10-07 12:33:52', 1755, 'Le Blanc-Mesnil', 'Pameungpeuk', 'Talane', 2, 401, 2347);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (18, 'Nangasuri Airport', 'Delta', '2023-10-13 18:39:30', 1803, 'Wuxia', 'Longtang', 'Zoomcast', 1, 230, 4788);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (19, 'Laredo International Airport', 'United', '2024-03-22 23:10:57', 797, 'Bukonyo', 'Dordrecht', 'Avamm', 4, 170, 2850);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (20, 'Koinambe Airport', 'Spirit', '2024-09-03 08:49:54', 556, 'Čelinac', 'Sernovodsk', 'Lajo', 5, 483, 2078);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (21, 'Kugluktuk Airport', 'United', '2024-02-08 21:09:23', 1880, 'Koltushi', 'Corredoura', 'Babbleopia', 1, 891, 2339);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (22, 'Arthur N Neu Airport', 'Alaska', '2024-04-19 19:48:19', 1455, 'Lukolela', 'Chelgard', 'Photofeed', 5, 124, 3318);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (23, 'Big Trout Lake Airport', 'Spirit', '2023-09-14 23:59:05', 1916, 'Jiuxian', 'Allen', 'Dynabox', 5, 805, 3295);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (24, 'Republic Airport', 'American', '2024-03-25 00:11:41', 1114, 'Vicuña', 'Piedra Blanca', 'Oloo', 3, 527, 1227);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (25, 'Baramita Airport', 'Spirit', '2023-11-17 10:07:43', 776, 'Lunel', 'Dĩ An', 'Gabtype', 3, 577, 1185);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (26, 'Essen Mulheim Airport', 'Alaska', '2023-10-26 16:54:55', 1202, 'Los Palacios', 'Moch', 'Jatri', 1, 202, 3927);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (27, 'Kandla Airport', 'Alaska', '2024-02-08 15:26:16', 1845, 'Mae Chan', 'Xiaoxiang', 'Twitterworks', 1, 211, 3886);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (28, 'Al Ghaidah International Airport', 'United', '2024-06-19 01:52:57', 404, 'Debe', 'Putou', 'Yambee', 5, 767, 3421);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (29, 'Selfridge Air National Guard Base Airport', 'American', '2023-10-26 16:26:33', 919, 'Ketitang Wetan', 'Moca', 'Tagopia', 4, 875, 1247);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (30, 'Jataí Airport', 'Alaska', '2023-10-20 08:12:01', 955, 'Concepción', 'Novo Selo', 'Babbleset', 4, 721, 2415);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (31, 'Pike County-Hatcher Field', 'Alaska', '2024-02-06 15:36:11', 742, 'Cruz', 'Sankanan', 'Tagfeed', 5, 655, 1512);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (32, 'Cork Airport', 'Delta', '2023-11-19 21:13:58', 1120, 'Popovi', 'Xinquan', 'Realblab', 2, 733, 1427);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (33, 'Nekemte Airport', 'Southwest', '2024-07-13 00:41:16', 776, 'Fagersta', 'Zykovo', 'Leenti', 4, 171, 3079);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (34, 'İncirlik Air Base', 'Alaska', '2023-12-20 19:50:51', 295, 'Köping', 'Marietta', 'Brainsphere', 2, 258, 2103);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (35, 'Rogers Municipal Airport-Carter Field', 'Alaska', '2023-10-14 17:30:50', 808, 'Henghe', 'Ma‘bar', 'Riffpedia', 3, 180, 3255);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (36, 'Ayapel Airport', 'American', '2024-08-30 22:55:39', 1308, 'Cañas', 'Cravo Norte', 'Blogspan', 1, 779, 1092);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (37, 'Awassa Airport', 'Alaska', '2023-11-14 09:55:46', 461, 'Nevel’', 'Krasnyy Oktyabr’', 'Feedfire', 3, 566, 2797);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (38, 'Akiachak Airport', 'United', '2024-05-27 14:19:44', 184, 'Stockholm', 'Sagopshi', 'Eadel', 5, 214, 4693);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (39, 'Oki Airport', 'United', '2024-01-01 11:20:08', 747, 'Makilala', 'Shisandaogou', 'Avamba', 5, 380, 1236);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (40, 'Pincher Creek Airport', 'American', '2024-04-14 04:33:37', 672, 'Longju', 'San Remigio', 'Rhycero', 3, 250, 4570);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (41, 'Lake Rudolf Airport', 'Alaska', '2024-07-17 19:50:59', 1303, 'Krikil', 'Shchastya', 'Eare', 1, 467, 2033);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (42, 'Akron Canton Regional Airport', 'Spirit', '2024-07-05 23:29:43', 204, 'El Águila', 'Yantang', 'Livetube', 5, 725, 3475);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (43, 'Surgut Airport', 'JetBlue', '2024-01-14 13:22:44', 162, 'New York City', 'Morales', 'Devbug', 1, 613, 2075);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (44, 'Hawkins Field', 'Southwest', '2024-06-02 21:03:19', 1634, 'Sawang Daen Din', 'Puerto Nariño', 'Skipstorm', 1, 394, 4117);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (45, 'Contadora Airport', 'United', '2024-01-31 18:05:11', 268, 'København', 'Portela', 'Centidel', 5, 185, 1390);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (46, 'Coulter Field', 'Delta', '2023-10-09 14:13:22', 1931, 'Daxing', 'Gala', 'Teklist', 2, 115, 3206);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (47, 'Ouro Sogui Airport', 'Spirit', '2024-04-20 04:58:18', 128, 'Krivaja', 'Tmourghout', 'Roomm', 2, 762, 3329);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (48, 'Ostrava Leos Janáček Airport', 'American', '2024-08-25 10:35:11', 148, 'Grand Rapids', 'Tracal', 'Cogibox', 5, 230, 3334);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (49, 'Zamperini Field', 'United', '2024-07-10 14:56:35', 1948, 'Kristianstad', 'Rostokino', 'Gabspot', 1, 617, 1696);
+insert into Reis (ID, Lennujaam, Transport, Kuupaev, Hind, Algus_Koht, Lõpp_Koht, Firma, FirmaRating, Ekskursioonid, Majutus) values (50, 'Mokuti Lodge Airport', 'JetBlue', '2024-07-09 08:49:56', 752, 'Nagua', 'Avranches', 'Shuffletag', 4, 696, 2488);
+
+
+SELECT 
+    Firma AS 'Firma/@Name',
+    (SELECT 
+        Transport AS 'Transport/@Type',
+        (SELECT 
+            Lennujaam AS 'Lennujaam/@Name',
+            Kuupaev,
+            Hind,
+			Firma,
+            Algus_Koht,
+            Lõpp_Koht,
+            FirmaRating,
+			Ekskursioonid,
+			Majutus
+         FROM Reis
+         WHERE Firma = OuterReis.Firma AND Transport = InnerReis.Transport
+         FOR XML PATH('Reis'), TYPE
+        )
+     FROM (SELECT DISTINCT Transport FROM Reis WHERE Firma = OuterReis.Firma) AS InnerReis
+     FOR XML PATH('Transport'), TYPE
+    )
+FROM 
+    (SELECT DISTINCT Firma FROM Reis) AS OuterReis
+FOR XML PATH('Firma'), ROOT('Reisid');
